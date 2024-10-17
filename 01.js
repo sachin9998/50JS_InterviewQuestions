ec6d0347f7152b04658bd8a62c25951ff15fb5dd
Sachin Alam <sachinalam4216@gmail.com>
2024-10-17 12:27:47 +0530

    first question done

--- 01.js
+++ 01.js
@@ -1,11 +1,25 @@
-// ====> Longest word in string
-// Q] Write a function findLongestWord that takes a string as input and returns the longest word in string. If there are multiple longest words, return the first one encountered.
+//* ---------------------------------------------------------
+//* Programming Question: Longest Word in a String
+//* ---------------------------------------------------------
+
+//? Q: Write a function findLongestWord that takes a string as input and returns the longest word in the string. If there are multiple longest words, return the first one encountered.
+
+//* Constraints:
+//? The input string may contain alphabetic characters, digits, spaces, and punctuation.
+//? The input string is non-empty.
+//? The input string may contain multiple words separated by spaces.
+
+//* Note:
+//? If the input string is empty or contains only whitespace, the function should return an false.
+//? The function should ignore leading and trailing whitespace when determining the longest word.
 
 console.log(
   findLongestWord(
     "Watch IPL on sonymax live to know longest six hitter batter from team india"
   )
 );
+
+// Brute Force Approach
 
 function findLongestWord(str) {
   if (str.trim().length === 0) {
@@ -30,3 +44,35 @@ function findLongestWord(str) {
 }
 
 // Answer - sonymax
+
+// Optimal Approach
+const findLongestWord2 = (str) => {
+  if (str.trim().length === 0) {
+    return false;
+  }
+
+  words = str.split(" ");
+  words = words.sort((a, b) => b.length - a.length);
+  console.log(words);
+  // return words.at(-1);
+  return words[0];
+};
+
+console.log(
+  findLongestWord2(
+    "Watch Thapa Technical javascript awesomethapatechnical course on youtube"
+  )
+);
+
+// Reduce Method
+const findLongestWord3 = (str) => {
+  str = str.split(" ");
+
+  return str.reduce((accumulator, currentWord) => (currentWord.length > accumulator.length ? currentWord : accumulator), "")
+};
+
+console.log(
+  findLongestWord3(
+    "Watch Thapa Technical javascript awesomethapatechnical course on youtube"
+  )
+);
