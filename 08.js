38fc756f367ac4b56c250c6b7dece5eee07ceb4e
Sachin Alam <sachinalam4216@gmail.com>
2024-10-26 18:37:15 +0530

    working on 9

--- 08.js
+++ 08.js
@@ -1,13 +1,21 @@
-
-
 const factorial = (num) => {
-    let fact = 1;
+  let fact = 1;
 
-    for(let i = 1; i <= num; i++) {
-        fact = fact * i;
-    }
+  for (let i = 1; i <= num; i++) {
+    fact = fact * i;
+  }
 
-    return fact;
-}
+  return fact;
+};
 
 console.log(factorial(5));
+
+for (var i = 0; i < 4; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 4; i++) {
+  console.log(i);
+}
+
+console.log("Value of ", i); // 4
