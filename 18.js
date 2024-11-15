772e4977dd1615282e91b387e94d584d89fcc0bd
Sachin Alam <sachinalam4216@gmail.com>
2024-11-15 10:44:50 +0530

    18

--- 18.js
+++ 18.js
@@ -1,9 +1,7 @@
 // Write a function to check if a character is uppercase or lowercase.
 
 const isUpperCase = (char) => {
-  if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
-    return true;
-  }
+  return char === char.toUpperCase();
 };
 
 const isLowerCase = (char) => {
